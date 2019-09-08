5280733a45c6898da4252216c0d5a10c14a1acf7
niovanna2 <54962609+niovanna2@users.noreply.github.com>
2019-09-08 11:57:50 -0400

    Levels and scene scripts

---
+++ Assets/Nicole/SceneLoadingScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadingScript : MonoBehaviour
+{
+    public int sceneIndex; //Current scenes index number
+    Transform triggerPos; //The position of the trigger
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        triggerPos = gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        LoadNextLevel();
+    }
+
+    public void LoadNextLevel()
+    {
+        if(sceneIndex == 1)
+        {
+            SceneManager.LoadSceneAsync("TestScene2", LoadSceneMode.Additive);
+            Debug.Log("Level Loaded");
+            SceneManagerScript.TriggerHeight = triggerPos;
+            gameObject.SetActive(false);
+        }
+        else if(sceneIndex == 2)
+        {
+            SceneManager.LoadSceneAsync("TestScene", LoadSceneMode.Additive);
+            Debug.Log("Level Loaded");
+            SceneManagerScript.TriggerHeight = triggerPos;
+            gameObject.SetActive(false);
+        }
+    }
+}
