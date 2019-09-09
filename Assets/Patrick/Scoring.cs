6a83275939652d7904642727a67eab0d25425e3c
DESKTOP-06UCPHN\Patrick Ngoka <rickyspiderman2@yahoo.com>
2019-09-09 00:59:26 -0400

    Scoring

---
+++ Assets/Patrick/Scoring.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scoring : MonoBehaviour
+{
+    //Variables
+    public Transform playerMovement;
+    public Text playerScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerScore.text = playerMovement.position.ToString;
+    }
+}
