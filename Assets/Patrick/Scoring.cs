f5acb1cd0ef920f98f551ab83d1b0f166293f2f2
DESKTOP-06UCPHN\Patrick Ngoka <rickyspiderman2@yahoo.com>
2019-09-11 19:56:46 -0400

    Scorng

--- Assets/Patrick/Scoring.cs
+++ Assets/Patrick/Scoring.cs
@@ -6,8 +6,13 @@ using UnityEngine.UI;
 public class Scoring : MonoBehaviour
 {
     //Variables
-    public Transform playerMovement;
-    public Text playerScore;
+   // public Transform playerMovement;
+    //public Text playerScore;
+    public GameObject startPosition;
+    public GameObject currentPosition;
+    int scoreDistance = 0;
+    public float distance;
+    public Text currentScore;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +22,18 @@ public class Scoring : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        playerScore.text = playerMovement.position.ToString();
+        distance = Vector3.Distance(currentPosition.transform.position, startPosition.transform.position);
+        //  playerScore.text = playerMovement.position.ToString();
+        currentScore.text = "Current Score: " + scoreDistance.ToString();
+        
+        GetDistance();
+    }
+
+    void GetDistance()
+    {
+        if (distance > scoreDistance)
+        {
+            scoreDistance = (int)distance;
+        }
     }
 }
