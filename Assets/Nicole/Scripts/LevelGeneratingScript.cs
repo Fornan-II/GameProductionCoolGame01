2af70edc9047a48a62b445c90bfb0ec63283165a
niovanna2 <54962609+niovanna2@users.noreply.github.com>
2019-09-11 19:20:32 -0400

    Restart function and randomized levels

---
+++ Assets/Nicole/Scripts/LevelGeneratingScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneratingScript : MonoBehaviour
+{
+    public GameObject triangleObject;
+
+    public GameObject enemyObject;
+
+    public GameObject ammoObject;
+
+    private int randNum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        randNum = Random.Range(1, 5);
+
+        if(randNum == 1 || randNum == 4)
+        {
+            Instantiate(triangleObject, transform.position, transform.rotation);
+        }
+        else if(randNum == 2)
+        {
+            Instantiate(enemyObject, transform.position, transform.rotation);
+        }
+        else if(randNum == 3)
+        {
+            Instantiate(ammoObject, transform.position, transform.rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Nicole/Scripts/RestartScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RestartScript : MonoBehaviour
+{
+    public Text countdownText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("LevelReset"); //Should probably add this to the player death script
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator LevelReset() //When envoked, wait a few seconds before resetting
+    {
+        countdownText.text = "Restarting in 5 seconds.";
+        yield return new WaitForSeconds(1f);
+        countdownText.text = "Restarting in 4 seconds.";
+        yield return new WaitForSeconds(1f);
+        countdownText.text = "Restarting in 3 seconds.";
+        yield return new WaitForSeconds(1f);
+        countdownText.text = "Restarting in 2 seconds.";
+        yield return new WaitForSeconds(1f);
+        countdownText.text = "Restarting in 1 second.";
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadSceneAsync("Master Game Scene");
+    }
+}
