f84c84252adccb08bb5b89eabd03f6b48a1af031
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2019-09-13 12:07:05 -0400

    Fixed bug w/ AI movement
    AI would occasionally get stuck if they got confused by numerous collisions

--- Assets/Maxen/AI/MovementVariations/HorizontalMover.cs
+++ Assets/Maxen/AI/MovementVariations/HorizontalMover.cs
@@ -9,19 +9,34 @@ public class HorizontalMover : AIMoveScript
     public float lerpRate = 0.7f;
 
     protected float preferredYLevel;
+    protected Collider2D _col;
 
     public LayerMask movementReversingLayers;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _col = GetComponent<Collider2D>();
+    }
 
     protected virtual void Start()
     {
         preferredYLevel = transform.localPosition.y;
     }
 
+    public Vector2 velocityMirror;
     public override void ProcessMovement(float deltaTime)
     {
         base.ProcessMovement(deltaTime);
-        
+
+        velocityMirror = _rb.velocity;
+        if(Mathf.Abs(_rb.velocity.sqrMagnitude) < 0.01)
+        {
+            movingRight = !movingRight;
+        }
+
         //Calculate moveVelocity
+        //
         Vector2 newVelocity = Vector2.left + Vector2.up * (preferredYLevel - transform.localPosition.y);
         newVelocity = newVelocity.normalized * moveSpeed;
         if(movingRight)
@@ -30,14 +45,6 @@ public class HorizontalMover : AIMoveScript
         }
         newVelocity = Vector2.Lerp(_rb.velocity, newVelocity, lerpRate * Time.timeScale);
         _rb.velocity = newVelocity;
-    }
-
-    protected virtual void OnCollisionEnter2D(Collision2D collision)
-    {
-        //https://answers.unity.com/questions/50279/check-if-layer-is-in-layermask.html
-        if (movementReversingLayers == (movementReversingLayers | (1 << collision.gameObject.layer)))
-        {
-            movingRight = !movingRight;
-        }
+        //
     }
 }
