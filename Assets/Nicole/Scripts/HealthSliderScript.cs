669688f22a7c12acc4b0a494f9c92f286add58f2
niovanna2 <54962609+niovanna2@users.noreply.github.com>
2020-02-17 11:55:12 -0500

    Health bar added

---
+++ Assets/Nicole/Scripts/HealthSliderScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSliderScript : MonoBehaviour
+{
+    private Slider healthSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthSlider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (PlayerScript.Instance)
+        {
+            healthSlider.value = PlayerScript.Instance.CurrentHealth;
+        }
+    }
+}
